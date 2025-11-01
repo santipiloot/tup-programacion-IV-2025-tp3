1184a427e57818a7517a990a288c708d69e935e3
Santino Exequiel Pilot <santinoexequielpilot0@gmail.com>
2025-11-01 19:09:23 -0300

    Agrega endpoint en el modulo conductores con el metodo PUT, para modificar conductores existentes

--- backend/controllers/conductores.js
+++ backend/controllers/conductores.js
@@ -33,6 +33,34 @@ const conductorController = {
             console.error(error)
             res.status(500).json({ success: false, message: "Error al crear el conductor" })
         }
+    },
+    update: async (req, res) => {
+        try {
+            const id = Number(req.params.id)
+            const { nombre, apellido, dni, licencia, vencimiento_licencia } = req.body;
+
+            await Conductor.update(nombre, apellido, dni, licencia, vencimiento_licencia, id);
+
+            res
+                .status(200)
+                .json({
+                    success: true,
+                    data: {
+                        id,
+                        nombre,
+                        apellido,
+                        dni,
+                        licencia,
+                        vencimiento_licencia
+                    }
+                })
+
+        } catch (error) {
+            console.error(error);
+            res
+                .status(500)
+                .json({ success: false, message: "Error al actualizar el conductor"})
+        }
     }
 }
 
