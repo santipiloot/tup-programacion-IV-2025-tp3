72283b4d0a0b1a8fc45d82390df579e8a7f0a3cd
Santino Exequiel Pilot <santinoexequielpilot0@gmail.com>
2025-10-31 23:01:08 -0300

    Agrega que el campo patente en vehiculos sea unico

--- backend/database.sql
+++ backend/database.sql
@@ -2,48 +2,53 @@
 CREATE DATABASE db-tp3;
 
 -- Tabla de usuarios
-CREATE TABLE usuarios (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  nombre VARCHAR(45) NOT NULL,
-  email VARCHAR(45) NOT NULL UNIQUE,
-  password_hash VARCHAR(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `usuarios` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `password_hash` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email_UNIQUE` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
 
 -- Tabla de conductores
-CREATE TABLE conductores (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  nombre VARCHAR(45) NOT NULL,
-  apellido VARCHAR(45) NOT NULL,
-  dni VARCHAR(15) NOT NULL UNIQUE,
-  licencia VARCHAR(20),
-  fecha_vencimiento_licencia DATE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `conductores` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) NOT NULL,
+  `apellido` varchar(45) NOT NULL,
+  `dni` varchar(15) NOT NULL,
+  `licencia` varchar(20) DEFAULT NULL,
+  `fecha_vencimiento_licencia` date DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `dni_UNIQUE` (`dni`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
 
 -- Tabla de vehiculos
-CREATE TABLE vehiculos (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  marca VARCHAR(45) NOT NULL,
-  modelo VARCHAR(45) NOT NULL,
-  patente VARCHAR(10) NOT NULL,
-  anio INT NOT NULL,
-  capacidad_carga DECIMAL(10,2) UNSIGNED NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `vehiculos` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `marca` varchar(45) NOT NULL,
+  `modelo` varchar(45) NOT NULL,
+  `patente` varchar(10) NOT NULL,
+  `anio` int NOT NULL,
+  `capacidad_carga` decimal(10,2) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `patente_UNIQUE` (`patente`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
 
 -- Tabla de viajes
-CREATE TABLE viajes (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  vehiculo_id INT NOT NULL,
-  conductor_id INT NOT NULL,
-  fecha_salida DATE NOT NULL,
-  fecha_llegada DATE DEFAULT NULL,
-  origen VARCHAR(45) NOT NULL,
-  destino VARCHAR(45) NOT NULL,
-  kilometros DECIMAL(10,2) NOT NULL,
-  observaciones TEXT DEFAULT NULL,
-  FOREIGN KEY (conductor_id) REFERENCES conductores(id)
-    ON DELETE RESTRICT
-    ON UPDATE RESTRICT,
-  FOREIGN KEY (vehiculo_id) REFERENCES vehiculos(id)
-    ON DELETE RESTRICT
-    ON UPDATE RESTRICT
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `viajes` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `vehiculo_id` int NOT NULL,
+  `conductor_id` int NOT NULL,
+  `fecha_salida` date NOT NULL,
+  `fecha_llegada` date NOT NULL,
+  `origen` varchar(45) NOT NULL,
+  `destino` varchar(45) NOT NULL,
+  `kilometros` decimal(10,2) NOT NULL,
+  `observaciones` text NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_viajes_conductores_idx` (`conductor_id`),
+  KEY `fk_viajes_vehiculos_idx` (`vehiculo_id`),
+  CONSTRAINT `fk_viajes_conductores` FOREIGN KEY (`conductor_id`) REFERENCES `conductores` (`id`),
+  CONSTRAINT `fk_viajes_vehiculos` FOREIGN KEY (`vehiculo_id`) REFERENCES `vehiculos` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
